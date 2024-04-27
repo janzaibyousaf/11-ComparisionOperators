fb277ca5fba398eca940e86c5494cf5e08ce1c0a
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-04-27 11:49:37 +0500

    Add files via upload

---
+++ 11-ComparisonOperators/app.js
@@ -0,0 +1,30 @@
+var education = prompt("Enter Your Education")
+
+if( education !== "matric"){
+
+console.log("Eligible!")
+
+}
+
+var age = 21
+if(age > 20){
+    console.log('Eligible')
+}
+
+var age = 20
+if(age < 21){
+    console.log("Eligible")
+}
+
+var age = 10
+
+if(age <= 10){
+
+    console.log('Eligible')
+}
+
+var age = 25
+
+if(age >= 19){
+console.log('Eligible')
+}
